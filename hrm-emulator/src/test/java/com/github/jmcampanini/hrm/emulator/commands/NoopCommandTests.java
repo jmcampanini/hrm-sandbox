67040adb3006e60f957e179fac1378a61ac323dc
Javier Campanini <jcampanini@palantir.com>
2017-01-11 21:31:52 +0800

    clean up noop test

--- hrm-emulator/src/test/java/com/github/jmcampanini/hrm/emulator/commands/NoopCommandTests.java
+++ hrm-emulator/src/test/java/com/github/jmcampanini/hrm/emulator/commands/NoopCommandTests.java
@@ -2,10 +2,7 @@ package com.github.jmcampanini.hrm.emulator.commands;
 
 import com.github.jmcampanini.hrm.emulator.Processor;
 import com.github.jmcampanini.hrm.emulator.ProgramEndSignal;
-import com.github.jmcampanini.hrm.emulator.Thing;
 import org.junit.Test;
-
-import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -14,14 +11,11 @@ import static org.mockito.Mockito.*;
  */
 public class NoopCommandTests {
 
-    private static final Thing THING_A = Thing.of("a");
-
-    private final NoopCommand command = NoopCommand.INSTANCE.INSTANCE;
+    private final NoopCommand command = NoopCommand.INSTANCE;
     private final Processor processor = mock(Processor.class, RETURNS_DEEP_STUBS);
 
     @Test
     public void increments_the_counter() throws ProgramEndSignal {
-        when(this.processor.worker().thing()).thenReturn(Optional.of(THING_A));
         this.command.execute(this.processor);
         verify(this.processor.programCounter()).increment();
     }
